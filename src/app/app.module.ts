4776251cd566155e35b4c046f83961cd929c574a
samsif <samet.development@gmail.com>
2021-12-03 10:12:37 +0100

    add primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {PrimeNGConfig} from "primeng/api";
+import {ButtonModule} from "primeng/button";
+import {RippleModule} from "primeng/ripple";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    RippleModule
   ],
-  providers: [],
+  providers: [PrimeNGConfig],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
